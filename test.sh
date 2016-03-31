883db5c4e3e06c6a51af23d025f741ff7e9acdef
Hoang Xuan Phu <phuhimself@phunehehe.net>
2016-03-31 21:08:18 +0800

    simplify test

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -efuo pipefail
-
-PATH=$(nix-build --no-out-link '<nixpkgs>' --attr cabal-install)/bin:$PATH
-
-nix-channel --update
-nix-shell --run 'cabal configure --enable-tests'
-cabal build
